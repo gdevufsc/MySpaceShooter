e62d94297ff6bdf907e594347a117f4664bdf30b
Caina <cainacc@hotmail.com>
2017-12-27 20:51:45 -0200

    Uploading do projeto

---
+++ Assets/GameOver.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//A biblioteca abaixo foi incluida para usar o GetActiveScene e o LoadScene;
+//using UnityEngine.SceneManagement;
+
+public class LoadNaColisao : MonoBehaviour {
+
+    //criando objeto s da classe Scene
+    //Scene s;
+
+    //pGOT é uma abreviação de positionGameOverText.
+    Vector2 pGOT;
+
+    private void Start()
+    {
+        //instanciando s, colocando nele a cena atual
+        //s = SceneManager.GetActiveScene();
+
+        //pGOT é relativo ao pai, que será o Canvas. .5f,.5f vai representar o meio da tela.
+        pGOT = new Vector2(.5f, .5f);
+    }
+
+    /// <summary> Teste para o comando LoadScene
+    /// private void Update()
+    ///{  
+    ///    if(Input.GetKey(KeyCode.R))
+    ///        SceneManager.LoadScene(s.buildIndex);
+    ///}
+    /// </summary>
+
+private void Update()
+    {
+         
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        //SceneManager.LoadScene(s.buildIndex);
+        MakeGameOverText();
+        DestroiCoisas();
+        //PleaseStopInvoking = true;
+    }
+
+    void DestroiCoisas()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
+    }
+    
+    void MakeGameOverText()
+    {
+        //cria objeto canv e atribui o objeto Canvas que foi taggeado como Canvas
+        GameObject canv = GameObject.FindGameObjectWithTag("Canvas");
+        //print(canv); //para testar se o canv foi instanciado;
+
+        // instancia prefab da pasta Resources com nome GameOverText
+        GameObject c = Instantiate(Resources.Load("GameOverText")) as GameObject;
+
+        //põe o c como filho do canv
+        c.transform.SetParent(canv.transform);
+
+        //cria um objeto cRectTransform para poder posicionar o c
+        RectTransform cRectTransform = c.GetComponent<RectTransform>();
+
+        //posiciona o c usando a posição pGOT
+        cRectTransform.anchoredPosition = pGOT;
+    }
+}
---
+++ Assets/SpawnStuff.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStuff : MonoBehaviour {
+
+    //O é o vetor de Objetos a serem spawnados;
+    GameObject[] O;
+    //ob armazena os objetozinhos clonados
+    GameObject ob;
+
+    //t é o tempo entre spawns
+    float t;
+
+    //min e max são os limites da tela
+    float min, max;
+
+    // é a posição em y para o spawn
+    float y;
+
+    //v é o vetor velocidade para os objetos spawnados
+    Vector2 v;
+
+    void Start () {
+        //Preenche o vetor O encontrando objetos na cena com a tag Respawn
+        O = GameObject.FindGameObjectsWithTag("Respawn");
+        /// testar O
+        ///print(O[0]);
+        ///print(O[1]);
+        ///print(O[2]);
+        ///print(O[3]);
+
+        t = 2f; 
+
+        min = -3.5f;
+        max = 3.5f;
+
+        y = transform.position.y;
+
+        v = new Vector2(0, -3);
+
+        InvokeRepeating("SpawnFunction", 2f, t);        
+    }
+	
+    void SpawnFunction ()
+    {
+
+        //define x aleatoriamente
+        float x = Random.Range(min, max);
+
+        // p é o vetor posição para instanciar o objeto
+        Vector3 p = new Vector3(x, y, 0);
+
+        //i identificará qual elemento do vetor será instanciado. É definido aleatoriamente
+        int i = Random.Range(0, 3);
+
+        //instancia objeto do vetor e armazena em ab, com posição p e rotação zerada
+        ob = Instantiate(O[i], p, Quaternion.Euler(Vector3.zero));
+
+        //atribui velocidade v ao objeto ob instanciado
+        ob.GetComponent<Rigidbody2D>().velocity = v;
+
+        //destroi ob em 5 segundos para que não fique pesando na memoria, depois de ter saído da tela.
+        Destroy(ob, 5);
+    }
+	
+	void Update () {
+	    
+	}
+}
